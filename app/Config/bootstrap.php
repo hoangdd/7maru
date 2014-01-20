19bb0d0a0c4a36904d577a71cc55a19d707097aa
Meob0nmat <datmeo1211@gmail.com>
2014-01-20 10:04:59 +0700

    Commit code Linux, Config, Debug

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -86,6 +86,7 @@ Cache::config('default', array('engine' => 'File'));
  *
  * ));
  */
+CakePlugin::load('DebugKit');
 Configure::write('Dispatcher.filters', array(
 	'AssetDispatcher',
 	'CacheDispatcher'
--- app/Config/core.php
+++ app/Config/core.php
@@ -222,13 +222,13 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'hwgfvwcvwvcwcvwcwucqwwvwvwv');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
-
+	Configure::write('Security.cipherSeed', '23565425865934696054673767567');
+	Configure::write('debug', 1);
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
  * Will append a query string parameter containing the time the file was modified. This is
