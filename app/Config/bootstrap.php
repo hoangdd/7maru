8a336f496f96b6221bf96f3078025afa1e2f41f3
hoangdd <ddhoang107@gmail.com>
2014-01-29 02:37:50 +0700

    enable file log

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -96,13 +96,16 @@ Configure::write('Dispatcher.filters', array(
  * Configures default file logging options
  */
 App::uses('CakeLog', 'Log');
-CakeLog::config('debug', array(
-	'engine' => 'File',
-	'types' => array('notice', 'info', 'debug'),
-	'file' => 'debug',
-));
-CakeLog::config('error', array(
-	'engine' => 'File',
-	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
-	'file' => 'error',
-));
+
+// --Enable custom debug -hoangdd
+
+// CakeLog::config('debug', array(
+// 	'engine' => 'File',
+// 	'types' => array('notice', 'info', 'debug'),
+// 	'file' => 'debug',
+// ));
+// CakeLog::config('error', array(
+// 	'engine' => 'File',
+// 	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
+// 	'file' => 'error',
+// ));
