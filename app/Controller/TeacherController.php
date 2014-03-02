11541561d47dc6f3a3b86197e614acac94fff4a1
reddevil138 <reddevil138@gmail.com>
2014-03-03 00:51:31 +0700

    fix register teacher controller

--- app/Controller/TeacherController.php
+++ app/Controller/TeacherController.php
@@ -9,10 +9,11 @@ class TeacherController extends AppController {
 	function Register(){
 
         $error = array();
-        $user_re_ex='/^[A-Za-z]+\.[A-Za-z]+[0-9]{0,2}$/';
+        //$user_re_ex='/^[A-Za-z]+\_[A-Za-z]+[0-9]$/'; 
+        $user_re_ex = '/^[A-Za-z]\w+$/';
         //$pass_re_ex='/^.*(?=.{7,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).*$/';
         $pass_re_ex = '/^\w+$/';
-        $email_re_ex ='[a-z0-9\\._]*[a-z0-9_]@[a-z0-9][a-z0-9\\-\\.]*[a-z0-9]\\.[a-z]{2,6}$' ;
+        $email_re_ex ='[a-z0-9\\._]*[a-z0-9_]@[a-z0-9][a-z0-9\\-\\.]*[a-z0-9]\\.[a-z]{2,6}$';
 		//If has data
 		if($this->request->is('post')){
 			$data = $this->request->data;
@@ -25,25 +26,31 @@ class TeacherController extends AppController {
                 4:  max long
                 5:  is used
             */
-    
+            $check_user= true;
+            
 			if(!isset($data['username'])){
                 $error['username'][0] ='Username is equal null.';
+                $check_user = false;
 			}
             
 			if(empty($data['username'])){
                 $error['username'][1] ='Username is empty.';
+                $check_user = false;
 			}	
             else{
                 if(!preg_match($user_re_ex,$data['username'])){
                     $error['username'][2] ='Username is not match form.'; 
+                    $check_user = false;
                 }
 
                 if(strlen($data['username']) < 6){
                     $error['username'][3] ='Username is too short.';
+                    $check_user = false;
                 }
 
                 if(strlen($data['username']) > 30){
                     $error['username'][4] ='Username is too long.';
+                    $check_user = false;
                 }
 
                 $res = $this->User->find('all',array(
@@ -55,6 +62,7 @@ class TeacherController extends AppController {
                     //$error['username'] ='Username chua ton tai!';
                 }else{
                     $error['username'][5] ='Username is exist.';
+                    $check_user = false;
                 }
             }
             //=================================
@@ -67,24 +75,30 @@ class TeacherController extends AppController {
                 3:  min short
                 4:  max long
             */
+            $check_user_password = true;
             if(!isset($data['password'])){
                 $error['password'][0] ='Password is equal null.';
+                $check_user = false;
 			}
             
 			if(empty($data['password'])){
                 $error['password'][1] ='Password is empty.';
+                $check_user = false;
 			}
             else{
                 if(!preg_match($pass_re_ex,$data['password'])){
                     $error['password'][2] ='Password is not match form.'; 
+                    $check_user = false;
                 }
 
                 if(strlen($data['password']) < 8){
                     $error['password'][3] ='Password is too short.';
+                    $check_user = false;
                 }
 
                 if(strlen($data['password']) > 30){
                     $error['password'][4] ='Password is too long.';
+                    $check_user = false;
                 }
             }
             
@@ -96,14 +110,17 @@ class TeacherController extends AppController {
             */
             if(!isset($data['retypepassword'])){
                 $error['retypepassword'][0] ='Password is equal null.';
+                $check_user = false;
 			}
             
 			if(empty($data['retypepassword'])){
                 $error['retypepassword'][1] ='Password is empty.';
+                $check_user = false;
 			}
             else{
                 if(strcmp($data['retypepassword'],$data['password'])!=0){
                     $error['retypepassword'][2] ='Password and RetypePassword are not equal.';
+                    $check_user = false;
                 }
             }
             
@@ -115,36 +132,63 @@ class TeacherController extends AppController {
             */
             if(!isset($data['firstname'])){
                 $error['firstname'][0] ='First name is equal null.';
+                $check_user = false;
 			}
             
 			if(empty($data['firstname'])){
                 $error['firstname'][1] ='First name is empty.';
+                $check_user = false;
 			}	
             else{
 
                 if(strlen($data['firstname']) > 30){
                     $error['firstname'][2] ='First name is too long.';
+                    $check_user = false;
                 }
             }
             //==================================
             if(!isset($data['lastname'])){
                 $error['lastname'][0] ='Last name is equal null.';
+                $check_user = false;
 			}
             
 			if(empty($data['lastname'])){
                 $error['lastname'][1] ='Last name is empty.';
+                $check_user = false;
 			}	
             else{
 
                 if(strlen($data['lastname']) > 30){
                     $error['lastname'][2] ='Last name is too long.';
+                    $check_user = false;
                 }
             }
+            //check email
+           if(!isset($data['mail'])){
+                $error['mail'][0] ='Email is equal null.';
+                $check_user = false;
+			}
+			if(empty($data['mail'])){
+                $error['mail'][1] ='Email is empty.';
+                $check_user = false;
+			}
             //=================================
+            
+            $check_teacher = true;
+            //check bank_account
+            if(!isset($data['bank_account'])){
+                $error['bank_account'][0] ='Bank account is equal null.';
+                $check_teacher = false;
+			}
+			if(empty($data['bank_account'])){
+                $error['bank_account'][1] ='Bank account is empty.';
+                $check_teacher = false;
+			}
+            //=================================
+            
 			//xong check
 			// tien hanh luu du lieu vao Model
             $data['id'] =$data['username'].''.$data['password'];
-            echo $data['id'];
             //save data of teacher
             /*
             *   Bank_acount, office, description
@@ -155,8 +199,10 @@ class TeacherController extends AppController {
                 'office'    =>      $data['office'],
                 'description'   =>  $data['description'],
             );
-			$this->Teacher->create($data_teacher);
-			$this->Teacher->save();
+            if($check_teacher==true){
+                $this->Teacher->create($data_teacher);
+                $this->Teacher->save();
+            }
             
             //save data of user
             /*
@@ -173,9 +219,12 @@ class TeacherController extends AppController {
                 'mail'  =>      $data['mail'],
                 'phone_number'  =>  $data['phone_number'],
                 'date_of_birth'  =>  $data['date_of_birth'],
+                'user_type' =>  1,
             );
-            $this->User->create($data_user);
-            $this->User->save();
+            if($check_user==true){
+                $this->User->create($data_user);
+                $this->User->save();
+            }
 		}
         $this->set('error', $error);
 	}
