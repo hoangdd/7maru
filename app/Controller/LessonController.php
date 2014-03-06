7987ff3d86ee2fe0c27f56bac5115e44fecda5f8
Meob0nmat <datmeo1211@gmail.com>
2014-03-06 20:39:04 +0700

    linhtinh

--- app/Controller/LessonController.php
+++ app/Controller/LessonController.php
@@ -12,14 +12,33 @@ class LessonController extends AppController {
 	function Create(){
         //  $this->loadModel('Category');
         $categories =  $this->Category->find('all');
-        debug($categories);
+//        debug($categories);
         $this->set('categories',$categories);
         
         if($this->request->is('post')){
             $data = $this->request->data;
             debug($data);
-            if(isset($data['copyright'])){
-                $this->Session->setFlash('Checked');
+            $error = array(); //error that return to client;
+            // check if copyright check box had checked yet?
+            if(!isset($data['copyright'])){
+                $this->Session->setFlash('Please Check copyright checkbox!!!');
+                $error['copyright'] = 'Please confirm your copyright';               
+            }
+            
+            // check if Lesson name is empty
+            if(!$data['name']){
+                $error['name'] = 'Lesson name do not suppose to be empty';
+            }
+            
+            //check if Lesson Description is empty
+            if($data['desc']==''){
+                $error['desc'] = 'Lesson Description do not suppose to be empty';
+            }
+            
+            if(count($error)){
+                $this->set('error',$error);
+                debug($error);
+                $this->set('data',$data);
             }
         }
 	}
