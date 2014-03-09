1363349097012b3d987ab99033894e966e8a99bf
vietkungfu <vietbktn@gmail.com>
2014-03-10 00:39:50 +0700

    Lesson manage

--- app/Model/Lesson.php
+++ app/Model/Lesson.php
@@ -1,5 +1,12 @@
 <?php
 
 class Lesson extends AppModel {
-	public $name = 'Coma';
+	public $useTable = 'comas';
+        public $primaryKey = 'coma_id';
+//        public $belongsTo = array(
+//            'User' => array(
+//            'className' => 'User',
+//            'foreignKey' => 'author',
+    ////        )
+    //    );
 }
