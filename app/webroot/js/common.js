6ebe74f5850655225e01dc3d4823cdf6956bb94a
HoangDD <ddhoang107@gmail.com>
2014-04-19 18:22:44 +0700

    Auto logout @Dat

--- app/webroot/js/common.js
+++ app/webroot/js/common.js
@@ -1,10 +1,48 @@
+if(typeof(idle_time) === 'undefined' ) idle_time = 60*1000;
+var flag = false;
+// var window_focus = ;
+var isTyping = false;
+var isMouseMoving = false;
 function idle(){
 	$('body').html('');
 	$.ajax({
-		'url' : "/7maru/Login/logout"
+		'url' : "/7maru/Login/logout",
+		complete : function(){
+			window.location = "/7maru/Login";
+		}
 		});
-	window.location = "/7maru/Login";
+	// console.log("die");
 }
-$(window).blur('focus', function(){
-	setTimeout(idle, 30*1000);
+$(window).focus(function(){
+	if(flag){
+		idle();
+	}
+	// window_focus = true;
 });
+timeout = setTimeout(forceLogout, idle_time);
+$(window).blur('focus', function(){
+	resetTimeout();
+	// window_focus = false;
+});
+$(document).on('mousemove',function(){
+	resetTimeout();
+})
+$(document).on('keydown',function(){
+	resetTimeout();
+})
+
+function resetTimeout(){
+	if(flag){
+		idle();
+	} else {
+		clearTimeout(timeout);
+		timeout = setTimeout(forceLogout, idle_time);
+	}
+}
+
+function forceLogout(){
+	flag = true;
+	if(document.hasFocus()){
+		idle();
+	}
+}
