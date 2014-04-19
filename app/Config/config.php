41d750f9a9c6877ed7bb93edb89f50071c34410d
reddevil138 <reddevil138@gmail.com>
2014-04-19 10:41:48 +0700

    update

--- app/Config/config.php
+++ app/Config/config.php
@@ -199,7 +199,7 @@ Configure::write('dataFile', array(
 
 Configure::write('command', array(
 	'pdf2swf' => array(
-		'pdf2swf %s -o %s'
+		'C:/SWFTools/pdf2swf.exe %s -o %s'
 		),
 
 	'doc2pdf' => array(
